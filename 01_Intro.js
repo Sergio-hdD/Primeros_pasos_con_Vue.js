10aa1d5fc7f893f4ba17e35566cad1c7456f3166
Sergio-hdD <sergio-hdd@hotmail.com>
2021-09-15 17:19:11 -0300

    Computed para tomar cambios y, para el ejemplo, sumar totales

--- 01_Intro.js
+++ 01_Intro.js
@@ -1,4 +1,4 @@
-const app = new Vue({
+const app = new Vue({//esta llave denota que es un array
     el: '#app', //detecto el id = app
     data:{//en data abro un objeto, para trabajar con datos
        titulo: 'Hola mundo con Vue',
@@ -7,7 +7,8 @@ const app = new Vue({
            {nombre:'Pera', cantidad:15},
            {nombre:'Banana', cantidad:0}
         ],
-        nuevaFruta: '' //si al inspeccionar en console pongo "app.nuevaFruta" se muestra lo que ingresa por el input
+        nuevaFruta: '', //si al inspeccionar en console pongo "app.nuevaFruta" se muestra lo que ingresa por el input
+        total: 0 //inicializo total en 0
     },
     methods: {
        agregarFruta(){
@@ -16,5 +17,14 @@ const app = new Vue({
            });
            this.nuevaFruta = '' //limpio el input
        }   
+    },
+    computed:{//Cada vez que haya una modificación
+        sumarCantidades(){//Se ejecuta sin necesidad de llamarlo en cada lugar, solo lo llamo donde quiero que retorne
+            this.total = 0 ; //Arranca en 0
+            for(fruta of this.frutas){
+                this.total = this.total + fruta.cantidad;
+            }
+            return this.total;
+        }
     }
 })//instancio vue, es decir llamo a la librería que copie en el scrip "<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>" en el html
