deb05ec757a35820c61354e84f3d41ead9981afe
Sergio-hdD <sergio-hdd@hotmail.com>
2021-09-15 14:53:42 -0300

    Agregando elementos al array

--- 01_Intro.js
+++ 01_Intro.js
@@ -11,7 +11,9 @@ const app = new Vue({
     },
     methods: {
        agregarFruta(){
-           console.log('diste clic');
+           this.frutas.push({
+               nombre: this.nuevaFruta, cantidad: 20
+           })
        }   
     }
 })//instancio vue, es decir llamo a la librería que copie en el scrip "<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>" en el html
