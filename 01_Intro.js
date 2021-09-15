46a2429521735bebd20003bcd4e27e66b0700b3f
Sergio-hdD <sergio-hdd@hotmail.com>
2021-09-14 21:48:21 -0300

    Array con v-for

--- 01_Intro.js
+++ 01_Intro.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el: '#app', //detecto el id = app
     data:{//en data abro un objeto, para trabajar con datos
-       titulo: 'Hola mundo con Vue'
+       titulo: 'Hola mundo con Vue',
+       frutas: ['Manzana','Pera','Banana']
     }
 })//instancio vue, es decir llamo a la librería que copie en el scrip "<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>" en el html
