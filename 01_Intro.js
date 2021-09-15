c6a32a032fdea3ca1c289de8b592a27f56408b49
Sergio-hdD <sergio-hdd@hotmail.com>
2021-09-14 21:01:05 -0300

    Hola mundo

---
+++ 01_Intro.js
@@ -0,0 +1,6 @@
+const app = new Vue({
+    el: '#app', //detecto el id = app
+    data:{//en data abro un objeto, para trabajar con datos
+       titulo: 'Hola mundo con Vue'
+    }
+})//instancio vue, es decir llamo a la librería que copie en el scrip "<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>" en el html
