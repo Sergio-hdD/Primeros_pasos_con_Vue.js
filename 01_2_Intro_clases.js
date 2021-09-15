9956f62e8a0a71f6fa2e88fcb13dcd1740f7ef92
Sergio-hdD <sergio-hdd@hotmail.com>
2021-09-15 18:21:28 -0300

    Cambiando dinamicamente el fondo

--- 01_2_Intro_clases.js
+++ 01_2_Intro_clases.js
@@ -4,5 +4,6 @@ const app = new Vue({
         fondo: 'bg-warning'
     },
     methods: {   
+
     }
 })//instancio vue, es decir llamo a la librería que copie en el scrip "<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>" en el html
